ce22cc89463f53b067ffb69db6c255727b53739e
azni <azni@gft.com>
2022-01-23 22:04:35 +0100

    correcting base url in auth service

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,41 +1,48 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/";
+const API_URL = "http://35.157.21.203:8081/";
 
 class AuthService {
-  async login(username, password) {
-    const response = await axios.post(API_URL + "authenticate", {username, password});
+	async login(username, password) {
+		const response = await axios.post(API_URL + "authenticate", {
+			username,
+			password,
+		});
 
-    try {
-        if (response.data.accessToken) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
-        return 'Success!'
-    } catch(err) {
-        return err;
-    }
-  }
+		try {
+			if (response.data.accessToken) {
+				localStorage.setItem("user", JSON.stringify(response.data));
+			}
+			return "Success!";
+		} catch (err) {
+			return err;
+		}
+	}
 
-  logout() {
-    localStorage.removeItem("user");
-  }
+	logout() {
+		localStorage.removeItem("user");
+	}
 
-  async register( username, password, firstName, lastName) {
-    const response = await axios.post(API_URL + "signUp", {username, password, firstName, lastName})
+	async register(username, password, firstName, lastName) {
+		const response = await axios.post(API_URL + "signUp", {
+			username,
+			password,
+			firstName,
+			lastName,
+		});
 
-    try {
-      if(response.data.id) {
-        
-      }
-      return 'Success!'
-    } catch(err) {
-      return err;
-    }
-  }
+		try {
+			if (response.data.id) {
+			}
+			return "Success!";
+		} catch (err) {
+			return err;
+		}
+	}
 
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
-  }
+	getCurrentUser() {
+		return JSON.parse(localStorage.getItem("user"));
+	}
 }
 
 export default new AuthService();
